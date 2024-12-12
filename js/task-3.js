a8b51ba62114136dcd47681be34153b80f727dc7
Maryna Bobryk <m.bobryk@web.de>
2024-12-12 16:19:51 +0100

    add tasks

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+// function filterArray(numbers, value){
+//     let numbersArray = [];
+//     for(let i = 0; i < numbers.length; i++){
+//         if (numbers[i] > value){
+//             numbersArray.push(numbers[i]);
+//         }
+//     }
+//     return numbersArray;
+// }
+
+
+function filterArray(numbers, value){
+    let numbersArray = [];
+    for(number of numbers){
+        if (number > value){
+            numbersArray.push(number);
+        }
+    }
+    return numbersArray;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
